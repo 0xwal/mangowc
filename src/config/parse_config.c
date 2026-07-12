cbee022ddf6362b8f0b484bf872fc51608d44e47
0xWal <18g3@e.0xw.link>
2026-07-12 14:43:34 +0300

    feat: focusdir using index :sparkles:

--- src/config/parse_config.c
+++ src/config/parse_config.c
@@ -4853,11 +4853,15 @@ FuncType parse_func_name(char *func_name, Arg *arg, char *arg_value,
 		func = group_focus;
 		(*arg).i = parse_circle_direction(arg_value);
 	} else if (strcmp(func_name, "focusdir") == 0) {
-		(*arg).i = parse_direction(arg_value);
-		(*arg).i2 = parse_direction_mode(arg_value2);
+		if (arg_value && arg_value[0] && isdigit(arg_value[0])) {
+			(*arg).ui = (uint32_t)atoi(arg_value);
+			(*arg).i = INDEX;
+		} else {
+			(*arg).i = parse_direction(arg_value);
+			(*arg).i2 = parse_direction_mode(arg_value2);
+		}
 
 		func = focus_direction;
-		(*arg).i = parse_direction(arg_value);
 	} else if (strcmp(func_name, "focus_window_or_workspace") == 0) {
 		func = focus_window_or_workspace;
 		(*arg).i = parse_direction(arg_value);
